e0f0840ce24582595e60c7194a96bbe7f363260d
Branden Keck <bmkeck62@gmail.com>
2017-11-06 02:42:11 -0500

    Refactored Code and Implemented Tests

--- Del4Test.java
+++ Del4Test.java
@@ -1,8 +1,6 @@
 //Java imports
 import java.lang.*;
-import java.util.logging.*;
-import java.util.concurrent.TimeUnit;
-import java.util.List;
+import java.util.ArrayList;
 
 //JUnit imports
 import org.junit.*;
@@ -27,8 +25,68 @@ public class Del4Test {
     
     // Initial test to ensure that the new Element instance isn't null
     @Test
-    public void testLaboonCoinExists() {
-		assertNotNull(_l);
+    public void testElementExists() {
+		assertNotNull(_e);
+    }
+	
+	
+	/**
+	*
+	BEGIN TESTS
+	The twelve required software tests begin here...
+	*
+	*/
+
+	
+	/*
+	TEST 1:
+	Ensure that a simple valid string returns a non-empty array list
+	*/
+	
+    @Test
+    public void testSimpleInputValid() {
+		ArrayList<String> t = new ArrayList<String>();
+		boolean checkMe;
+		
+		t = _e.getAbbreviations("Laboon");
+		
+		checkMe = t.isEmpty();
+		
+		assertFalse(checkMe);
+    }
+	
+	/*
+	TEST 2:
+	Ensure that a simple NON-VALID string returns an EMPTY array list
+	*/
+	
+    @Test
+    public void testSimpleInputNotValid() {
+		ArrayList<String> t = new ArrayList<String>();
+		boolean checkMe;
+		
+		t = _e.getAbbreviations("Bill");
+		
+		checkMe = t.isEmpty();
+		
+		assertTrue(checkMe);
+    }
+	
+	
+	/*
+	TEST 3:
+	Ensure that the program is case-insensitive by asserting two identical
+	inputs with different cases yield the same abbreviation
+	*/
+	
+    @Test
+    public void testNotCaseSensitive() {
+		ArrayList<String> t1 = new ArrayList<String>();
+		ArrayList<String> t2 = new ArrayList<String>();
+		
+		t1 = _e.getAbbreviations("Laboon");
+		t2 = _e.getAbbreviations("lAbOoN");
+		assertEquals(t1, t2);
     }
 	
 }
--- Element.java
+++ Element.java
@@ -12,6 +12,8 @@ import java.util.HashMap;
 import java.util.ArrayList;	//https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html
 
 public class Element{
+	//Declare globally used hashmap
+	static HashMap<String, String> hmap = new HashMap<String, String>();
 	
 	//Initiate a FileReader and BufferedReader object using the filePath argument
 	public static BufferedReader openFile(String path){
@@ -26,9 +28,9 @@ public class Element{
 		
 	}
 	
+	
 	//Fills hmap with the contents of elements.txt
-	public static HashMap<String, String> getElements(BufferedReader inFile){
-		HashMap<String, String> hmap = new HashMap<String, String>();
+	public static void fillGlobalElementMap(BufferedReader inFile){
 		String line;
 		
 		try{
@@ -40,82 +42,85 @@ public class Element{
 			}
 		}catch(IOException ioe){
 			System.out.println("ERROR: Element.java could not read elements.txt");
-			System.exit(2);
+			System.exit(3);
+		}
+	}
+	
+	// Creates the array list of atom abbreviations
+	// Returns the list for use in unit testing
+	public static ArrayList<String> getAbbreviations(String _line){
+		ArrayList<String> _a = new ArrayList<String>();
+		String testLine, result;
+		int startIndex = 0;
+		int endIndex = 2;
+		
+		// prints the current line
+		System.out.println(_line);
+		
+		//parse line for matching abbreviations using substring()
+		while(startIndex < _line.length()){
+			if(endIndex > _line.length()){endIndex=_line.length();}
+			testLine = _line.substring(startIndex, endIndex);
+			result = hmap.get(testLine);
+			
+			if(result != null){
+				_a.add(testLine);
+				startIndex = endIndex;
+				endIndex = startIndex + 2;
+			}else{
+				// if there is no abbreviation, return an empty list
+				if(endIndex - startIndex == 1){	
+					_a.clear();
+					return _a;
+				}else{
+					//try a 1 letter combination
+					endIndex = endIndex - 1;
+				}
+			}
 		}
 		
-		return hmap;
+		return _a;
+	}
+	
+	// Gets the element associated with a specific abbreviation
+	public static String getElement(String _e){
+			return hmap.get(_e);
 	}
 	
 	//Reads in a string from user input, attempts to parse all the individual characters and return that array
-	public static void run(BufferedReader inElements, BufferedReader inFile){
-		HashMap<String, String> hmap = new HashMap<String, String>();
+	public static void run(BufferedReader inFile){
+		ArrayList<String> abbreviations = new ArrayList<String>();
+		ArrayList<String> elements = new ArrayList<String>();
 		String line;
-		String testLine;
-		String result;
-		ArrayList<String> abbreviations;
-		ArrayList<String> elements;
-		int startIndex;
-		int endIndex;
-		
-		//Parse from elements.txt - updates hmap
-		hmap = getElements(inElements);
 		
 		try{
 			while((line = inFile.readLine()) != null){
-				abbreviations = new ArrayList<String>();
-				elements = new ArrayList<String>();
-				int flag = 0;
-				
-				//get rid of all non-alphabetical characters + case-insensitive
+				// removes of all non-alphabetical characters
+				// eliminates case sensitivity
 				line = line.replaceAll("[^A-Za-z]+","");	
-				line = line.toUpperCase();	
+				line = line.toUpperCase();
 				
-				System.out.println(line);
-				
-				//parse line for matching abbreviations using substring()
-				startIndex = 0;
-				endIndex = 1;
-				while(startIndex < line.length()){
-					testLine = line.substring(startIndex, endIndex);
-					result = hmap.get(testLine);
-					
-					if(result != null){
-						abbreviations.add(testLine);
-						elements.add(result);
-						startIndex = endIndex;
-					}else{
-						if(endIndex - startIndex == 2){	//there is no abbreviation
-							System.out.println("\nCould not create name '"+line+"' out of elements.");
-							flag = 1;	//don't print out a partial attempt
-							break;
-						}else{	//try a 2 letter combination
-							endIndex = endIndex + 1;
-						}
-					}
-				}
+				// get the abbreviations and reset elements list
+				abbreviations = getAbbreviations(line);
+				elements.clear();
 				
 				//Print out abbreviations line / I - N - AC
-				if(flag == 0){
-					int i = 0;
-					while(i < abbreviations.size()){ 
-						if(i == 0){
-							System.out.print("\n" + abbreviations.get(0));
-						}else{
-							System.out.print(" - " + abbreviations.get(i));
-						}
-						i = i + 1;
+				if(!abbreviations.isEmpty()){
+					System.out.print("\n" + abbreviations.get(0));
+					elements.add(getElement(abbreviations.get(0)));
+					
+					for (int i = 1; i < abbreviations.size(); i++) {
+						System.out.print(" - " + abbreviations.get(i));
+						elements.add(getElement(abbreviations.get(i)));
 					}
 					
-					//Print out elements line / Iodine - Nitrogen - Actinium
-					i = 0;
-					while(i < elements.size()){
-						if(i == 0){
-							System.out.print("\n" + elements.get(0));
-						}else{
-							System.out.print(" - " + elements.get(i));
-						}
-						i = i + 1;
+					System.out.print("\n" + elements.get(0));
+					for (int i = 1; i < elements.size(); i++) {
+						System.out.print(" - " + elements.get(i));
 					}
+					
+				}else{
+					System.out.println("\nCould not create name '"+line+"' out of elements.");
 				}
 				
 				
@@ -125,9 +130,6 @@ public class Element{
 			System.out.println("ERROR: Element.java could not read user input file");
 			System.exit(2);
 		}
-		
-		System.out.println("TEST: HMAP SIZE = " + hmap.size());	//TEST should be 118 from elements.txt
-
 	}
 	
 	public static void main(String[] args){
@@ -146,11 +148,14 @@ public class Element{
 		}
 		
 		//Open both the elements file and the user specified input
+		BufferedReader inUser = openFile(filePath);
 		BufferedReader inElements = openFile("elements.txt");
-		BufferedReader inUser = openFile(filePath); 
 		
-		//Parse input from user file
-		run(inElements, inUser);
+		// uses the imported element data to create a hashmap
+		fillGlobalElementMap(inElements);
+		
+		//runs the program using the user input
+		run(inUser);
 		
 		//Cleanup
 		try{
